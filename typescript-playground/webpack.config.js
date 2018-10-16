60fb5b42a38ab0692dd0133e2cb5633987ac6486
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-10-16 11:41:13 +0200

    Webpack

---
+++ typescript-playground/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './main.ts',
+    output: { filename: 'bundle.js' },
+    resolve: {
+        extensions: ['.ts']
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    },
+    mode: 'development'
+}
