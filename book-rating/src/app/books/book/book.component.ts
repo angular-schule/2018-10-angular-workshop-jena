fc2592912145a69bdf260a9ffbea87d120f81e74
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-10-17 14:35:05 +0200

    Sprache einstellen

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent implements OnInit {
 
@@ -14,14 +15,13 @@ export class BookComponent implements OnInit {
 
   constructor(private rs: BookRatingService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   rateUp() {
     const ratedBook = this.rs.rateUp(this.book);
     this.rate.emit(ratedBook);
   }
-  
+
   rateDown() {
     const ratedBook = this.rs.rateDown(this.book);
     this.rate.emit(ratedBook);
