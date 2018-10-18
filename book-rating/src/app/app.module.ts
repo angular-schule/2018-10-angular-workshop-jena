9f92f49085777ddd5df9ca744efbf020af645c29
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-10-18 16:34:42 +0200

    Lazy Loading

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -6,7 +6,6 @@ import localeDe from '@angular/common/locales/de';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,7 @@ import { BooksModule } from './books/books.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BooksModule
+    // BooksModule // weg wegen Lazy Loading
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'de' }
